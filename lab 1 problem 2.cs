ad2dad87cc717d5b8929db6208a9b96d22693e3f
mhpranta12 <60335123+mhpranta12@users.noreply.github.com>
2020-10-16 16:17:44 +0600

    Add files via upload

---
+++ lab 1 problem 2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1__problem_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number :");
+            String x; 
+            x= Console.ReadLine();
+            Console.WriteLine(x);
+
+            String y;
+            y = Console.ReadLine();
+            Console.WriteLine(y);
+            
+
+
+
+        }
+    }
+}
