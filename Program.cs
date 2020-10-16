65258d491c72ee93d0b738ea7ab564aa5f8553ab
mhpranta12 <60335123+mhpranta12@users.noreply.github.com>
2020-10-16 16:19:50 +0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,19 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab_1_Problem_7
+namespace Lab1_Problem_8
 {
     class Program
     {
         static void Main(string[] args)
         {
-           double width = 5.6;
-           double height = 8.5;
-           double area,perimeter;
-            area = height * width;
-            perimeter = 2*(height +width);
-            Console.WriteLine(area);
-            Console.WriteLine(perimeter);
+            int no1 = 25;
+            int no2=39;
+            if(no1!=no2)
+            {
+                Console.WriteLine(no1+"!="+no2);
+            }
+            if (no2>no1)
+            {
+                Console.WriteLine(no2 + ">" + no1);
+            }
+            if(no1<=no2)
+            {
+                Console.WriteLine(no1 + "<=" + no2);
+            }
         }
     }
 }
